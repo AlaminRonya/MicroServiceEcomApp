97a5fc5b521cd0c248c464e64cdc2cbc101197f9
AlaminRonya <hprony12@gmail.com>
2023-04-25 13:46:49 +0600

    Create controller and service for order-service

---
+++ order-service/src/main/java/com/alamin/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,37 @@
+package com.alamin.orderservice.service;
+
+import com.alamin.orderservice.dto.RequestOrderDTO;
+import com.alamin.orderservice.dto.RequestOrderLineItemDTO;
+import com.alamin.orderservice.interfaces.OrderService;
+import com.alamin.orderservice.model.Order;
+import com.alamin.orderservice.model.OrderLineItems;
+import com.alamin.orderservice.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final OrderRepository orderRepository;
+    @Override
+    public void placeOrder(RequestOrderDTO dto) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        final List<OrderLineItems> orderLineItems = dto.orderLineItemDTOS().stream().map(this::mapToDto).toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(RequestOrderLineItemDTO item) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setPrice(item.price());
+        orderLineItems.setSkuCode(item.skuCode());
+        orderLineItems.setQuantity(item.quantity());
+        return orderLineItems;
+    }
+}
